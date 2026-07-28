0f482bf5db67ed00f8ce59e69882251aefe23584
Bhushan Dobale <bhushandobale55@gmail.com>
2026-07-28 04:26:57 +0000

    conversionOpration

---
+++ basics/conversionOpration.js
@@ -0,0 +1,30 @@
+//let score = 33  //=> 33
+  
+//let  score ="33abc"  //=>NaN
+//let score = null //=> 0
+//let score = undefined  // => NaN
+//let score = true  //=> 1
+//let score = false // => 0
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+//let isLoggIn = 1
+
+//let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1=> true ; 0 => false 
+// "" => false
+// "hitesh" => true 
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber
+
+);
