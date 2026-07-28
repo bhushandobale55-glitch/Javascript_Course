f9296c23f5a3c5e9caf4fde482626501f654b996
Bhushan Dobale <bhushandobale55@gmail.com>
2026-07-28 03:46:32 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node.js, not browser
+//1. tc39.es for javascript documentation 
+  //2. mdn 
+
+console.log( 3 + 3 )//code readability should be high
+
+console.log("bhushan")
+
+let name ="bhushan"
+let age = 27
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value 
+//undefine =>
+//symbol => unique 
+
+//object 
+
+console.log(typeof unddefine);// Undefined
+console.log(typeof null); //Object 
+// jab hum  null lete hai datatype ke under to usaka output object ata hai 
